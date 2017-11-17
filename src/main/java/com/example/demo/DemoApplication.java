7d10ffe10ff1782a83b5124ec906c3bd61eda2d9
tanujoberoi <tanuj.oberoi@ioptibiz.com>
2017-11-17 20:54:04 +0530

    ds

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+
+
+	@RequestMapping(value="/")
+	//@ResponseBody
+	public String grt(){
+		return "index";
+	}
+
+
 }
+
+
+
+
